bd99baaf606a7ea2ab9977e30df45adc9bdfd805
reza goldouzian <reza.goldouzian@cern.ch>
2020-04-30 15:21:01 +0200

    crab config

---
+++ test/crab3_multicrab_2017Data.py
@@ -0,0 +1,43 @@
+data2017_samples = {}
+data2017_samples["2017_B_SinglePhoton"]=[    ["/SinglePhoton/Run2017B-31Mar2018-v1/MINIAOD"],    "data2017"]
+data2017_samples["2017_C_SinglePhoton"]=[    ["/SinglePhoton/Run2017C-31Mar2018-v1/MINIAOD"],    "data2017"]
+data2017_samples["2017_D_SinglePhoton"]=[    ["/SinglePhoton/Run2017D-31Mar2018-v1/MINIAOD"],    "data2017"]
+data2017_samples["2017_E_SinglePhoton"]=[    ["/SinglePhoton/Run2017E-31Mar2018-v1/MINIAOD"],    "data2017"]
+data2017_samples["2017_F_SinglePhoton"]=[    ["/SinglePhoton/Run2017F-31Mar2018-v1/MINIAOD"],    "data2017"]
+
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+
+    def submit(config):
+        res = crabCommand('submit', config = config)
+
+    from CRABClient.UserUtilities import config
+    config = config()
+
+    name = 'ExitedTopSamplesData'
+    config.General.workArea = 'crab_'+name
+    config.General.transferLogs = False
+    config.General.transferOutputs = True
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = 'IIHE.py'
+    config.Data.inputDBS = 'global'
+#    config.Data.splitting = 'LumiBased'
+#    config.Data.splitting = 'FileBased'
+    config.Data.lumiMask = '/afs/cern.ch/work/r/rgoldouz/IIHETree/miniAOD/slc7/CMSSW_10_2_13/src/UserCode/IIHETree/test/data/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'
+    config.Site.storageSite = 'T3_US_NotreDame'
+    config.JobType.allowUndistributedCMSSW = True
+    config.JobType.pyCfgParams = ['DataProcessing=data2017']
+    config.JobType.inputFiles   = ['data']
+    config.Data.splitting = 'Automatic'
+#    config.Data.unitsPerJob = 40
+    config.Data.publication = False
+
+    for key, value in data2017_samples.items():
+        config.General.requestName = key
+        config.Data.inputDataset = value[0][0]
+        config.Data.outLFNDirBase = '/store/user/rgoldouz/' + name
+#        config.Data.outputDatasetTag = sample
+        submit(config)
+
+
---
+++ test/crab3_multicrab_2017MC.py
@@ -0,0 +1,62 @@
+mc2017_samples = {}
+mc2017_samples["2017_GJetsHT100To200"]=[    ["/GJets_DR-0p4_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8_v2/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_GJetsHT200To400"]=[    ["/GJets_DR-0p4_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_v2/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_GJetsHT400To600"]=[    ["/GJets_DR-0p4_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8_v2/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_GJetsHT600ToInf"]=[    ["/GJets_DR-0p4_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+        
+mc2017_samples["2017_QCDHT100to200_1"]=[    ["/QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT100to200_2"]=[    ["/QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT200to300"]=[    ["/QCD_HT200to300_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT300to500"]=[    ["/QCD_HT300to500_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+#mc2017_samples["2017_QCDHT500to700"]=[    ["/QCD_HT500to700_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT700to1000"]=[    ["/QCD_HT700to1000_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT1000to1500"]=[    ["/QCD_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+#mc2017_samples["2017_QCDHT1500to2000"]=[    ["/QCD_HT1500to2000_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_QCDHT2000toInf"]=[    ["/QCD_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+        
+mc2017_samples["2017_DYJetsToLLM50"]=[    ["/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14_ext1-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_DYJetsToLLM10to50"]=[    ["/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_WJetsToLNu"]=[    ["/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"],    "mc2017"]
+        
+mc2017_samples["2017_WGPtG40to130"]=[    ["/WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v3/MINIAODSIM"],    "mc2017"]
+mc2017_samples["2017_WGPtG130"]=[    ["/WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+        
+#mc2017_samples["2017_tt"]=[    ["/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+#mc2017_samples["2017_ttG"]=[    ["/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"],    "mc2017"]
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+
+    def submit(config):
+        res = crabCommand('submit', config = config)
+
+    from CRABClient.UserUtilities import config
+    config = config()
+
+    name = 'ExitedTopSamplesMC'
+    config.General.workArea = 'crab_'+name
+    config.General.transferLogs = False
+    config.General.transferOutputs = True
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = 'IIHE.py'
+    config.Data.inputDBS = 'global'
+#    config.Data.splitting = 'LumiBased'
+#    config.Data.splitting = 'FileBased'
+#    config.Data.lumiMask = '/user/xgao/IIHENtuples/2017_TOP_prefire/CMSSW_10_2_13/src/UserCode/IIHETree/test/crab_20190516_data/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
+    config.Site.storageSite = 'T3_US_NotreDame'
+    config.JobType.allowUndistributedCMSSW = True
+    config.JobType.pyCfgParams = ['DataProcessing=mc2017']
+    config.JobType.inputFiles   = ['data']
+    config.Data.splitting = 'Automatic'
+#    config.Data.unitsPerJob = 20
+    config.Data.publication = False
+
+    for key, value in mc2017_samples.items():
+        print key
+        config.General.requestName = key
+        config.Data.inputDataset = value[0][0]
+        config.Data.outLFNDirBase = '/store/user/rgoldouz/' + name
+#        config.Data.outputDatasetTag = sample
+        submit(config)
+
+
